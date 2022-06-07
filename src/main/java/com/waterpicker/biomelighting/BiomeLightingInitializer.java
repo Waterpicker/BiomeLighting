4415e5e57389af25bb89221b37752120680f3ec1
Mysticpasta1 <ethan.cantrell@asub.edu>
2022-06-07 15:47:14 -0500

    biome lighting push

--- src/main/java/com/waterpicker/biomelighting/BiomeLightingInitializer.java
+++ src/main/java/com/waterpicker/biomelighting/BiomeLightingInitializer.java
@@ -1,23 +1,9 @@
 package com.waterpicker.biomelighting;
 
-import com.google.gson.JsonElement;
-import com.waterpicker.biomelighting.data.BiomeLightProvider;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.minecraft.server.packs.PackType;
-import net.minecraft.world.level.biome.Biomes;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.AddReloadListenerEvent;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-import java.util.function.BiConsumer;
-@Mod("biomelighting")
 public class BiomeLightingInitializer implements ModInitializer {
     @Override
     public void onInitialize() {
