3fe74eaba8038fe26e49ef2b95f27396c3c295f6
Mysticpasta1 <ethan.cantrell@asub.edu>
2022-06-09 01:30:58 -0500

    forge with datapacks

--- src/main/java/com/waterpicker/biomeskylighting/BiomeLightingSkyEngine.java
+++ src/main/java/com/waterpicker/biomeskylighting/BiomeLightingSkyEngine.java
@@ -1,6 +1,7 @@
 package com.waterpicker.biomeskylighting;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
 import net.minecraft.core.QuartPos;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.ChunkPos;
@@ -24,12 +25,12 @@ public class BiomeLightingSkyEngine extends SkyLightEngine {
 
         BlockGetter blockGetter = chunkSource.getChunkForLighting(chunkPos.x, chunkPos.z);
         if (blockGetter instanceof ChunkAccess chunkAccess) {
-            Biome biome = chunkAccess.getNoiseBiome(
+            Holder<Biome> biome = chunkAccess.getNoiseBiome(
                     QuartPos.fromBlock(blockPos.getX()),
                     QuartPos.fromBlock(blockPos.getY()),
                     QuartPos.fromBlock(blockPos.getZ())
             );
-            lightValue = BiomeMapLoader.getInstance().getLightValue(biome.getRegistryName()).orElse(0);
+            lightValue = BiomeMapLoader.getInstance().getLightValue(biome.value().getRegistryName()).orElse(0);
         }
 
         if (world.dimension() == Level.OVERWORLD) { //The Overworld is a use case
@@ -42,48 +43,4 @@ public class BiomeLightingSkyEngine extends SkyLightEngine {
             return Math.min(lightValue, propagatedLevel);
         }
     }
-
-//    @Override
-//    protected int computeLevelFromNeighbor(long sourceId, long targetId, int level) {
-//        int propagatedLevel = super.computeLevelFromNeighbor(sourceId, targetId, level);
-//        BlockPos blockPos = BlockPos.of(targetId);
-//        ChunkPos chunkPos = new ChunkPos(blockPos);
-//
-//        BlockGetter blockGetter = chunkSource.getChunkForLighting(chunkPos.x, chunkPos.z);
-//        if (blockGetter instanceof ChunkAccess chunkAccess) {
-//            Biome biome = chunkAccess.getNoiseBiome(
-//                    QuartPos.fromBlock(blockPos.getX()),
-//                    QuartPos.fromBlock(blockPos.getY()),
-//                    QuartPos.fromBlock(blockPos.getZ())
-//            );
-//
-//
-//            OptionalInt optionalInt = BiomeMapLoader.getInstance().getLightValue(biome.getRegistryName());
-//
-//            if (optionalInt.isPresent()) {
-//                int biomeLightLevel = optionalInt.getAsInt();
-//
-//                //This may look complex but basically propagate the biomeLightLevel value here!
-//                if (targetId != Long.MAX_VALUE && sourceId != Long.MAX_VALUE) {
-//                    int i2;
-//                    int l1;
-//                    int i = BlockPos.getX(sourceId);
-//                    int j = BlockPos.getY(sourceId);
-//                    int k = BlockPos.getZ(sourceId);
-//                    int l = BlockPos.getX(targetId);
-//                    int i1 = BlockPos.getY(targetId);
-//                    int j1 = BlockPos.getZ(targetId);
-//                    int k1 = Integer.signum(l - i);
-//                    Direction direction = Direction.fromNormal(k1, l1 = Integer.signum(i1 - j), i2 = Integer.signum(j1 - k));
-//                    if (direction == null) {
-//                        throw new IllegalStateException(String.format("Light was spread in illegal direction %d, %d, %d", k1, l1, i2));
-//                    }
-//                    boolean flag = i == l && k == j1;
-//                    boolean flag1 = flag && j > i1;
-//                    return flag1 && level == 0 && biomeLightLevel == 0 ? 0 : Math.max(1, biomeLightLevel);
-//                }
-//            }
-//        }
-//        return propagatedLevel;
-//    }
 }
